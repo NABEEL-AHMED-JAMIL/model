b2e46b74d776603fbd63fe60fede070e40a395d7
NABEEL-AHMED-JAMIL <nabeel.amd93@gmail.com>
2020-08-25 00:49:42 +0500

    custome server added for security share

---
+++ src/main/java/com/barco/model/repository/AppUserRepository.java
@@ -0,0 +1,24 @@
+package com.barco.model.repository;
+
+import com.barco.model.enums.Status;
+import com.barco.model.pojo.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+     Optional<AppUser> findByUsername(String username);
+
+     Optional<AppUser> findByUsernameAndStatus(String username, Status status);
+
+     AppUser findByUsernameAndStatusNot(String username, Status status);
+
+     Optional<AppUser> findByUsernameIgnoreCaseAndStatusNot(String username, Status status);
+
+     AppUser findByIdAndStatusNot(Long userId, Status status);
+
+}
