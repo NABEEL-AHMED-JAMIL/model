6e01a855d11be5c65cf0b45239f88e9385f350cd
adeel <adeel@mentoringminds.com>
2020-11-30 17:26:48 +0500

    ticket-49992737-1  paggination query for admin user

--- src/main/java/com/barco/model/util/QueryUtil.java
+++ src/main/java/com/barco/model/util/QueryUtil.java
@@ -37,4 +37,20 @@ public class QueryUtil {
         QueryUtil queryUtil = new QueryUtil();
         System.out.println(String.format(queryUtil.fetchSuperAdminUserListQuery(), 1026, 1026));
     }
+
+    public static String adminUsersList(boolean isCount) {
+        String selectPortion = "";
+        if(isCount) {
+            selectPortion = "select count(*), 'count' as result\n";
+        } else {
+            selectPortion = "select ap_us.id, ap_us.first_name || ' ' || ap_us.last_name as full_name, ap_us.username, ap_us.last_login_at,\n" +
+                    " auth.role, ap_us.status, ap_us.user_type";
+        }
+        String query  = selectPortion + " from app_user as ap_us\n" +
+                "  inner join user_authority as us_auth on ap_us.id = us_auth.user_id\n" +
+                "  inner join authority as auth on auth.id = us_auth.authority_id\n" +
+                "  inner join app_sub_user as ap_su on ap_su.parent_user_id = ap_us.created_by_id\n" +
+                "where ap_su.parent_user_id = ? group by ap_us.id, auth.role;";
+        return query;
+    }
 }
