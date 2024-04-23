6989fdc3e2be52516893675d0adc9c546244aa50
Nabeel Ahmed <nabeel.amd93@gmail.com>
2024-04-24 02:59:29 +0300

    Code refector
    Code refector

--- src/main/java/com/barco/model/repository/GenFormLinkSourceTaskTypeRepository.java
+++ src/main/java/com/barco/model/repository/GenFormLinkSourceTaskTypeRepository.java
@@ -2,6 +2,7 @@ package com.barco.model.repository;
 
 import com.barco.model.pojo.GenForm;
 import com.barco.model.pojo.GenFormLinkSourceTaskType;
+import com.barco.model.pojo.SourceTaskType;
 import com.barco.model.util.lookup.APPLICATION_STATUS;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,8 @@ import java.util.List;
 public interface GenFormLinkSourceTaskTypeRepository extends CrudRepository<GenFormLinkSourceTaskType, Long> {
 
     public Long countByGenFormAndStatusNot(GenForm genForm, APPLICATION_STATUS status);
+
+    public Long countBySourceTaskTypeAndStatusNot(SourceTaskType sourceTaskType, APPLICATION_STATUS status);
 
     public List<GenFormLinkSourceTaskType> findAllByIdInAndStatusNot(List<Long> ids, APPLICATION_STATUS status);
 
