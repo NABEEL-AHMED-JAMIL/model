4db0bca5d26d446f48b6e9218ecb4ad573bab269
Nabeel Ahmed <nabeel.amd93@gmail.com>
2024-05-23 16:42:22 +0300

    Update Credential Setting And Web Hook Component
    Update Credential Setting And Web Hook Component
    Code Refactor Added

--- src/main/java/com/barco/model/dto/request/CredentialRequest.java
+++ src/main/java/com/barco/model/dto/request/CredentialRequest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.google.gson.Gson;
 
+import java.util.List;
+
 /**
  * @author Nabeel Ahmed
  */
@@ -16,6 +18,7 @@ public class CredentialRequest extends RequestFilter {
     private Long type; // yes
     private Long status;
     private Object content; // yes
+    private List<Long> types;
     private SessionUser sessionUser;
 
     public CredentialRequest() {
@@ -59,6 +62,14 @@ public class CredentialRequest extends RequestFilter {
 
     public void setContent(Object content) {
         this.content = content;
+    }
+
+    public List<Long> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<Long> types) {
+        this.types = types;
     }
 
     public SessionUser getSessionUser() {
--- src/main/java/com/barco/model/repository/CredentialRepository.java
+++ src/main/java/com/barco/model/repository/CredentialRepository.java
@@ -3,12 +3,14 @@ package com.barco.model.repository;
 import com.barco.model.pojo.AppUser;
 import com.barco.model.pojo.Credential;
 import com.barco.model.util.lookup.APPLICATION_STATUS;
+import com.barco.model.util.lookup.CREDENTIAL_TYPE;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * @author Nabeel Ahmed
@@ -33,6 +35,8 @@ public interface CredentialRepository extends CrudRepository<Credential, Long> {
     public List<Credential> findAllByDateCreatedBetweenAndUsernameAndStatusNot(Date startDate, Date endDate, String username, APPLICATION_STATUS status);
 
     public List<Credential> findAllByCreatedByAndStatusNot(AppUser createdBy, APPLICATION_STATUS status);
+
+    public List<Credential> findAllByCreatedByAndTypeInAndStatusNot(AppUser createdBy, Set<CREDENTIAL_TYPE> types, APPLICATION_STATUS status);
 
     public List<Credential> findAllByIdIn(List<Long> ids);
 
